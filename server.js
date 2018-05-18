d7be47cbe43d80ae2cfa560905a4818bde6bec91
Preston Brown <pbrown@brown-house.net>
2018-05-18 09:28:57 -0400

    work on progressive web app, and add pace visual progress loader

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ app.use('/api', function(req, res) {
  */
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 }
 
 app.listen(port, () => console.log(`Express server listening on port ${port}`));
